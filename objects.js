81e349d22bd30f7aac1e629bd84b0efdcdd707ba
Esayas Gebre <dave183278@gmail.com>
2018-05-20 10:47:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {artistName: "songTitle"}
  
  function updatePlaylist(obj,artistname,songtitle) 
  {
-   playlist.artistname=songtitle
+   playlist[artistname]=songtitle
    return playlist
    
  }
